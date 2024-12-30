09e6b66aebd4e378156d203193a1eaded97ccdfb
franzsamilo <sssamilofranz@gmail.com>
2024-12-30 14:42:30 +0800

    [Fix] Tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#F4811E",
-        accent: "#2F9B47",
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
